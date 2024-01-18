a3285bfab8a3a8e6da517b904409231cdfba8501
ubaid naeem <117588645+ubaid2917@users.noreply.github.com>
2024-01-19 00:16:17 +0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const sessionSecret = 'hfhahhafajuamfaafamdfarnfnayrefabyfamfhuaeahfa';
 const emailUser = 'wolfstech25@gmail.com';
 const emailPassword = 'gsjsprtdyhrhlasa';
-
+const port = 3000;
 module.exports = {
     sessionSecret,
     emailUser,
